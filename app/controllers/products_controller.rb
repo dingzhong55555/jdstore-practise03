3ffd5a700e629a84d8e986c8ba6ca9ebb3471b06
dingzhong <dingzhong55555@gmail.com>
2017-07-22 21:15:27 +0800

    implement cart_total_price function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,15 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    redirect_to :back
+    flash[:notice] = "已成功加入购物车！"
+  end
 
 end
